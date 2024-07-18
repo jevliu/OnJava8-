a07cf59f1760ba4fe591750a88b9cfce5b6d3715
jevliu <jevliu@163.com>
2024-07-18 12:16:34 +0800

    重新抛出异常

---
+++ exceptions/src/Rethrowing.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author 刘季伟
+ * @implNote 展示如何重新抛出异常
+ * @since 2024/7/18 12:07:52
+ */
+public class Rethrowing {
+    public static void f() throws Exception {
+        System.out.println("originating the exception in f()");
+        throw new Exception("thrown from f()");
+    }
+
+    public static void g() throws Exception {
+        try{
+            f();
+        }catch (Exception e){
+            System.out.println("Inside g(), e.printStackTrace()");
+            e.printStackTrace(System.out);
+            throw e;
+        }
+    }
+
+    public static void h() throws Exception {
+        try {
+            f();
+        } catch (Exception e){
+            System.out.println("Inside h(), e.printStackTrace()");
+            e.printStackTrace(System.out);
+            throw (Exception) e.fillInStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            g();
+        } catch (Exception e) {
+            System.out.println("main: printStackTrace()");
+            e.printStackTrace(System.out);
+        }
+        try {
+            h();
+        } catch (Exception e) {
+            System.out.println("main: printStackTrace()");
+            e.printStackTrace(System.out);
+        }
+    }
+}
