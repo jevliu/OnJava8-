ae1b14865384551dcf95b8af4f67d9c304bd798e
jevliu <jevliu@163.com>
2024-07-18 15:56:54 +0800

    定义FileToWords.java

---
+++ streams/src/FileToWords.java
@@ -0,0 +1,15 @@
+import java.nio.file.*;
+import java.util.stream.*;
+import java.util.regex.Pattern;
+/**
+ * @author 刘季伟
+ * @implNote
+ * @since 2024/7/18 15:54:03
+ */
+public class FileToWords {
+    public static Stream<String> stream(String filePath) throws Exception {
+        return Files.lines(Paths.get(filePath))
+                .skip(1)
+                .flatMap(line -> Pattern.compile("\\W+").splitAsStream(line));
+    }
+}
