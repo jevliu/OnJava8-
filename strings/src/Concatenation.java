ac313f470062c2c2700ce160db8112723d19c80c
jevliu <jevliu@163.com>
2024-07-19 10:20:58 +0800

    '+'运算符重载

---
+++ strings/src/Concatenation.java
@@ -0,0 +1,12 @@
+/**
+ * @author 刘季伟
+ * @implNote + 运算符的重载
+ * @since 2024/7/19 10:07:44
+ */
+public class Concatenation {
+    public static void main(String[] args) {
+        String mango = "mango";
+        String s = "abc" + mango + "def" + 47;
+        System.out.println(s);
+    }
+}
